f8e0223b5bac97ae57051c47eb77275ad4a5821d
VictorMerinoITCBCN <2023_victor.merino@iticbcn.cat>
2024-11-20 15:54:42 +0100

    modified data base

--- db/db.sql
+++ db/db.sql
@@ -1,33 +1,44 @@
-CREATE TABLE IF NOT EXISTS User (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(255) NOT NULL,
-    lastName VARCHAR(255) NOT NULL,
-    class_group VARCHAR(50) NOT NULL,
-    email VARCHAR(255),
-    phone BIGINT
-);
-
-CREATE TABLE IF NOT EXISTS Teacher (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(255) NOT NULL,
-    lastName VARCHAR(255) NOT NULL,
-    email VARCHAR(255),
-    phone BIGINT
-);
-
-CREATE TABLE IF NOT EXISTS Subject (
+CREATE TABLE IF NOT EXISTS Group (
     id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(255) NOT NULL
 );
 
+CREATE TABLE IF NOT EXISTS Role (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS User (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    lastName VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE,
+    groupID INT NOT NULL,
+    roleID INT NOT NULL,
+    FOREIGN KEY (groupID) REFERENCES Group(id),
+    FOREIGN KEY (roleID) REFERENCES Role(id)
+);
+
+CREATE TABLE IF NOT EXISTS Subject (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    room VARCHAR(255) NOT NULL,
+    teacherID INT NOT NULL,
+    weekday INT NOT NULL,
+    startTime TIME NOT NULL,
+    endTime TIME NOT NULL,
+    FOREIGN KEY (teacherID) REFERENCES User(id)
+);
+
 CREATE TABLE IF NOT EXISTS Assistance (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    userID INT,
-    teacherID INT,
-    subjectID INT,
-    attendance_status ENUM('present', 'absent', 'justified'),
+    studentID INT NOT NULL,
+    teacherID INT NOT NULL,
+    subjectID INT NOT NULL,
+    attendance_status ENUM('present', 'absent', 'justified') NOT NULL,
     date DATETIME DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (userID) REFERENCES User(id),
-    FOREIGN KEY (teacherID) REFERENCES Teacher(id),
+    UNIQUE (studentID, subjectID, date),
+    FOREIGN KEY (studentID) REFERENCES User(id),
+    FOREIGN KEY (teacherID) REFERENCES User(id),
     FOREIGN KEY (subjectID) REFERENCES Subject(id)
 );
